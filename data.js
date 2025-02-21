27c89db56ab4ef9be8a8a3d9f70e4308ca0aa3b3
Peder Skou <peder.skou@gyldendal.no>
2025-02-21 10:05:03 +0100

    More content

--- data.js
+++ data.js
@@ -35,6 +35,7 @@ const cards = [
         title: "Ibsen",
         topic: "Norsk",
         subtopic: "Litteratur",
+        instruction: 'Henrik Ibsen levde fra 1828 til 1906.'
     },
     {
         title: "Kielland",
@@ -50,11 +51,13 @@ const cards = [
         title: "Lie",
         topic: "Norsk",
         subtopic: "Litteratur",
+        instruction: 'Rekk opp hånden hvis du har lest så mye som et punktum av Jonas Lie!'
     },
     {
         title: "Kvinnelige forfattere",
         topic: "Norsk",
         subtopic: "Litteratur",
+        instruction: 'Noen forfattere er kvinner. De er ikke like anerkjente som mennene, selv om de kan være vel så dyktige.'
     },
     {
         title: "De tunge årene",
@@ -67,139 +70,139 @@ const cards = [
         subtopic: "Norsk historie",
     },
     {
-        title : 'Legg sammen',
-        topic : 'Matematikk',
-        subtopic : 'Algebra'
+        title: 'Legg sammen',
+        topic: 'Matematikk',
+        subtopic: 'Algebra'
     },
     {
-        title : 'Trekk fra',
-        topic : 'Matematikk',
-        subtopic : 'Algebra'
+        title: 'Trekk fra',
+        topic: 'Matematikk',
+        subtopic: 'Algebra'
     },
     {
-        title : 'Gange',
-        topic : 'Matematikk',
-        subtopic : 'Algebra'
+        title: 'Gange',
+        topic: 'Matematikk',
+        subtopic: 'Algebra'
     },
     {
-        title : 'Dele',
-        topic : 'Matematikk',
-        subtopic : 'Algebra'
+        title: 'Dele',
+        topic: 'Matematikk',
+        subtopic: 'Algebra'
     },
     {
-        title : 'Nøtteoppgave',
-        topic : 'Matematikk',
-        subtopic : 'Algebra'
+        title: 'Nøtteoppgave',
+        topic: 'Matematikk',
+        subtopic: 'Algebra'
     },
     {
-        title : 'Vinkler i trekanter',
-        topic : 'Matematikk',
-        subtopic : 'Trigonometri'
+        title: 'Vinkler i trekanter',
+        topic: 'Matematikk',
+        subtopic: 'Trigonometri'
     },
     {
-        title : 'Sinus',
-        topic : 'Matematikk',
-        subtopic : 'Trigonometri'
+        title: 'Sinus',
+        topic: 'Matematikk',
+        subtopic: 'Trigonometri'
     },
     {
-        title : 'Cosinus',
-        topic : 'Matematikk',
-        subtopic : 'Trigonometri'
+        title: 'Cosinus',
+        topic: 'Matematikk',
+        subtopic: 'Trigonometri'
     },
     {
-        title : 'Tangens',
-        topic : 'Matematikk',
-        subtopic : 'Trigonometri'
+        title: 'Tangens',
+        topic: 'Matematikk',
+        subtopic: 'Trigonometri'
     },
     {
-        title : 'Likesidede trekanter',
-        topic : 'Matematikk',
-        subtopic : 'Trigonometri'
+        title: 'Likesidede trekanter',
+        topic: 'Matematikk',
+        subtopic: 'Trigonometri'
     },
     {
-        title : 'Addisjon med små tall',
-        topic : 'Matematikk',
-        subtopic : 'Pluss'
+        title: 'Addisjon med små tall',
+        topic: 'Matematikk',
+        subtopic: 'Pluss'
     },
     {
-        title : 'Addisjon med store tall',
-        topic : 'Matematikk',
-        subtopic : 'Pluss'
+        title: 'Addisjon med store tall',
+        topic: 'Matematikk',
+        subtopic: 'Pluss'
     },
     {
-        title : 'Addisjon med kjempestore tall',
-        topic : 'Matematikk',
-        subtopic : 'Pluss'
+        title: 'Addisjon med kjempestore tall',
+        topic: 'Matematikk',
+        subtopic: 'Pluss'
     },
     {
-        title : 'Nøtteoppgave',
-        topic : 'Matematikk',
-        subtopic : 'Pluss'
+        title: 'Nøtteoppgave',
+        topic: 'Matematikk',
+        subtopic: 'Pluss'
     },
     {
-        title : 'Låne',
-        topic : 'Matematikk',
-        subtopic : 'Minus'
+        title: 'Låne',
+        topic: 'Matematikk',
+        subtopic: 'Minus'
     },
     {
-        title : 'Oppsett i rutenett',
-        topic : 'Matematikk',
-        subtopic : 'Minus'
+        title: 'Oppsett i rutenett',
+        topic: 'Matematikk',
+        subtopic: 'Minus'
     },
     {
-        title : 'Nøtteoppgave',
-        topic : 'Matematikk',
-        subtopic : 'Minus'
+        title: 'Nøtteoppgave',
+        topic: 'Matematikk',
+        subtopic: 'Minus'
     },
     {
-        title : 'Ekte brøk',
-        topic : 'Matematikk',
-        subtopic : 'Brøk'
+        title: 'Ekte brøk',
+        topic: 'Matematikk',
+        subtopic: 'Brøk'
     },
     {
-        title : 'Uekte brøk',
-        topic : 'Matematikk',
-        subtopic : 'Brøk'
+        title: 'Uekte brøk',
+        topic: 'Matematikk',
+        subtopic: 'Brøk'
     },
     {
-        title : 'Minste fellesnevner',
-        topic : 'Matematikk',
-        subtopic : 'Brøk'
+        title: 'Minste fellesnevner',
+        topic: 'Matematikk',
+        subtopic: 'Brøk'
     },
     {
-        title : 'Utro brøk',
-        topic : 'Matematikk',
-        subtopic : 'Brøk'
+        title: 'Utro brøk',
+        topic: 'Matematikk',
+        subtopic: 'Brøk'
     },
     {
-        title : 'Bråk - brøk på svensk',
-        topic : 'Matematikk',
-        subtopic : 'Brøk'
+        title: 'Bråk - brøk på svensk',
+        topic: 'Matematikk',
+        subtopic: 'Brøk'
     },
     {
-        title : 'Potetprester',
-        topic : 'Historie',
-        subtopic : 'Opplysningstiden'
+        title: 'Potetprester',
+        topic: 'Historie',
+        subtopic: 'Opplysningstiden'
     },
     {
-        title : 'Den franske revolusjon',
-        topic : 'Historie',
-        subtopic : 'Opplysningstiden'
+        title: 'Den franske revolusjon',
+        topic: 'Historie',
+        subtopic: 'Opplysningstiden'
     },
     {
-        title : 'Renhold og hygiene',
-        topic : 'Historie',
-        subtopic : 'Opplysningstiden'
+        title: 'Renhold og hygiene',
+        topic: 'Historie',
+        subtopic: 'Opplysningstiden'
     },
     {
-        title : 'Religionens fall',
-        topic : 'Historie',
-        subtopic : 'Opplysningstiden'
+        title: 'Religionens fall',
+        topic: 'Historie',
+        subtopic: 'Opplysningstiden'
     },
     {
-        title : 'Opplysningen 180',
-        topic : 'Historie',
-        subtopic : 'Opplysningstiden'
+        title: 'Opplysningen 180',
+        topic: 'Historie',
+        subtopic: 'Opplysningstiden'
     }
 ];
 
